01ac3a887b70ecac7980f3f86692e6973c86b918
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-11-15 16:28:25 +0700

    Form components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -3,8 +3,8 @@
 import { createBoard } from '@/actions/create-board';
 import { useAction } from '@/hooks/use-action';
 
-import { FormInput } from './form-input';
-import { FormButton } from './form-button';
+import { FormInput } from '@/components/form/form-input';
+import { FormSubmit } from '@/components/form/form-submit';
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -24,10 +24,14 @@ export const Form = () => {
   return (
     <form action={onSubmit}>
       <div className='flex flex-col space-y-2'>
-        <FormInput errors={fieldErrors} />
+        <FormInput
+          id='title'
+          label='Board Title'
+          errors={fieldErrors}
+        />
       </div>
 
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 };
