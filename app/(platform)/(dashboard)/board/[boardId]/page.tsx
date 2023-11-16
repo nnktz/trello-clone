720de61f149b52ebcfda83b8739a6ce0879b1445
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-11-16 15:23:16 +0700

    List component

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,50 @@
-const BoardIdPage = () => {
-  return <div></div>;
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
+import { db } from '@/lib/db';
+
+import { ListContainer } from './_components/list-container';
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect('/select-org');
+  }
+
+  const list = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  });
+
+  return (
+    <div className='p-4 h-full overflow-x-auto'>
+      <ListContainer
+        boardId={params.boardId}
+        data={list}
+      />
+    </div>
+  );
 };
 
 export default BoardIdPage;
